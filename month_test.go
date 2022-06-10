f845ba7219b0639eac43939801baa3c66120dd48
young-win <young.zhu.2012@gmail.com>
2022-06-10 16:50:42 +0800

    date.MonthDays()

--- month_test.go
+++ month_test.go
@@ -10,3 +10,24 @@ func TestMonth_String(t *testing.T) {
 	t.Log(January.String())
 	t.Log(Month(13).String())
 }
+
+var monthDaysTests = []struct {
+	year, month, days int
+}{
+	{2011, 1, 31},  // January, first month, 31 days
+	{2011, 2, 28},  // February, non-leap year, 28 days
+	{2012, 2, 29},  // February, leap year, 29 days
+	{2011, 6, 30},  // June, 30 days
+	{2011, 12, 31}, // December, last month, 31 days
+}
+
+func TestDate_MonthDays(t *testing.T) {
+	for _, tt := range monthDaysTests {
+		date, _ := NewDateYMD(tt.year, tt.month, 1)
+		days := date.MonthDays()
+		if days != tt.days {
+			t.Errorf("got %d; expected %d for %d-%02d",
+				days, tt.days, tt.year, tt.month)
+		}
+	}
+}
--- year.go
+++ year.go
@@ -1,6 +1,10 @@
 package godate
 
+func isLeap(year int) bool {
+	return year%4 == 0 && (year%100 != 0 || year%400 == 0)
+}
+
 func (d Date) IsLeapYear() bool {
 	year := d.Year()
-	return year%4 == 0 && (year%100 != 0 || year%400 == 0)
+	return isLeap(year)
 }
