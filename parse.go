b0a04897f4c8ee12f624db8af6661e14534a0e1e
Young-Mac Pro <young.zhu.2012@gmail.com>
2022-08-07 21:46:18 +0800

    parse date

--- parse.go
+++ parse.go
@@ -19,32 +19,43 @@ func ParseString(s string) (Date, error) {
 		year  int
 		month Month
 		day   int
+
+		err error
 	)
 
 	var validDate = regexp.MustCompile(`\d{4}-\d{2}-\d{2}`)
 	if validDate.MatchString(s) {
-		values := strings.Split(s, "-")
-		//fmt.Println(values)
-		y, err := strconv.ParseInt(values[0], 10, 32)
+		year, month, day, err = parseStr1(s)
 		if err != nil {
-			return Date{}, fmt.Errorf("invalid year: %s", values[0])
+			return Date{}, err
 		}
-		year = int(y)
-
-		m, err := strconv.ParseInt(values[1], 10, 32)
-		if err != nil {
-			return Date{}, fmt.Errorf("invalid month: %s", values[1])
-		}
-		month = Month(m)
-
-		d, err := strconv.ParseInt(values[2], 10, 32)
-		if err != nil {
-			return Date{}, fmt.Errorf("invalid year: %s", values[2])
-		}
-		day = int(d)
 	}
 
 	return NewDateYMD(year, int(month), day)
+}
+
+func parseStr1(s string) (year int, month Month, day int, err error) {
+	values := strings.Split(s, "-")
+	//fmt.Println(values)
+	y, err := strconv.ParseInt(values[0], 10, 32)
+	if err != nil {
+		return 0, 0, 0, fmt.Errorf("invalid year: %s", values[0])
+	}
+	year = int(y)
+
+	m, err := strconv.ParseInt(values[1], 10, 32)
+	if err != nil {
+		return 0, 0, 0, fmt.Errorf("invalid month: %s", values[1])
+	}
+	month = Month(m)
+
+	d, err := strconv.ParseInt(values[2], 10, 32)
+	if err != nil {
+		return 0, 0, 0, fmt.Errorf("invalid year: %s", values[2])
+	}
+	day = int(d)
+
+	return year, month, day, nil
 }
 
 func Parse(v interface{}) (Date, error) {
