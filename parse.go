53836d2e2151a00ca3db9a2c06f8a1b19145c932
Young-Mac Pro <young.zhu.2012@gmail.com>
2022-08-07 17:27:01 +0800

    parse date

---
+++ parse.go
@@ -0,0 +1,18 @@
+package godate
+
+// 日期转换
+
+var formats = []string{
+	"YYYY-MM-DD", // 2021-07-26
+	"YYYY-MM-DD", // July 1st, 2021
+}
+
+func ParseString(s string) (Date, error) {
+
+	return NewDate(), nil
+}
+
+func Parse(v interface{}) (Date, error) {
+
+	return NewDate(), nil
+}
